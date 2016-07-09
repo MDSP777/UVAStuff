63f8031bc8c51c5efaaf631d233f5ecea46e3e8f
MDSP777 <mdsp777@outlook.com>
2016-07-09 13:29:08 +0800

    deleted stuff

--- src/TheMagicOfUnicode.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.Scanner;
-
-public class TheMagicOfUnicode {
-    public static void main(String[] args){
-        // \u000A System.out.println("Hello World"); 
-    }
-}
--- src/asdf.java
+++
@@ -1,6 +0,0 @@
-public class asdf {
-    public static void main(String[] args){
-        System.out.println("Double.NaN == Double.NaN: "+(Double.NaN==Double.NaN));
-        System.out.println("Double.NaN != Double.NaN: "+(Double.NaN!=Double.NaN));
-    }
-}
